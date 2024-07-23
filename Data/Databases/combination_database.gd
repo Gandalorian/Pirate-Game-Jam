009057ba9dade8f36d36e09dcd1315877f8fa620
Gandalorian <scheezofren@gmail.com>
2024-07-23 15:31:30 +0200

    Finished combination, for now

--- Data/Databases/combination_database.gd
+++ Data/Databases/combination_database.gd
@@ -17,32 +17,13 @@ func _ready():
 func get_item(ID):
 	return cache[ID + ".tres"]
 
-func get_valid_combinations(ingredients):
+func get_valid_combinations(ingredient)->Array[Combination]:
 	var valid_combinations:Array[Combination]
 	for combination in cache:
 		var combo = cache[combination]
 		var reqs = combo.requires
-		var andnot = combo.not_include
 		
-		var matches = 0
-		if reqs.has(ingredients[1]):
-			matches += 1
-		if reqs.has(ingredients[2]):
-			matches += 1
-		if reqs.has(ingredients[3]):
-			matches += 1
-		if reqs.has(ingredients[4]):
-			matches += 1
-		
-		if matches == 4:
-			valid_combinations = [combo]
-			return valid_combinations
-		
-		if matches >= 1:
-			if ingredients[1] and andnot.has(ingredients[1].type): continue
-			if ingredients[2] and andnot.has(ingredients[2].type): continue
-			if ingredients[3] and andnot.has(ingredients[3].type): continue
-			if ingredients[4] and andnot.has(ingredients[4].type): continue
+		if reqs.has(ingredient):
 			valid_combinations.append(combo)
 	
 	return valid_combinations
--- Scenes/Scripts/garden_screen.gd
+++ Scenes/Scripts/garden_screen.gd
@@ -1,11 +1,25 @@
 extends Node
 
 @onready var container = $Container
+@onready var spawnpoints_container = $"Container/Spawn points"
+
+var spawnpoints:Array[TextureButton] = []
 
 signal on_shop_button_pressed
 
 func _ready():
-	pass # Replace with function body.
+	for point_container in spawnpoints_container:
+		for child in point_container.get_children():
+			spawnpoints.append(child)
+
+func spawn_gatherables():
+	var amount_to_spawn = randi_range(3,spawnpoints.size())
+	var amount_spawned = 0
+	while(amount_spawned < amount_to_spawn):
+		var point = spawnpoints[randi_range(0,spawnpoints.size())]
+		if not point.item:
+			point.set_gaterable(ItemDatabase.get_random_gaterable())
+			amount_spawned += 1
 
 func set_visible(visibility):
 	container.visible = visibility
